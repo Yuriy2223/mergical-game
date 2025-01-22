f694901f01ee190999480bf141d02eed05835671
Yuriy <>
2025-01-22 18:08:57 +0200

    add header

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 import "./src/scss/style.scss";
-import "./src/js/swiper"
-import "./src/js/faq"
-
-
+import "./src/js/burger-menu";
+import "./src/js/swiper";
+import "./src/js/faq";
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,4 +1,4 @@
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+window.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   anchor.addEventListener("click", function (e) {
     e.preventDefault();
 
