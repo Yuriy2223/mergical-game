10cca98298dec0f12305afddff79eafb7b991390
Yuriy <>
2025-01-25 04:35:37 +0200

    add section reviewes

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,21 +2,36 @@ import Swiper from "swiper";
 import { Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 
-const swiper = new Swiper(".swiper-container", {
-  modules: [Navigation, Pagination],
-  // direction: "horizontal",
-  // loop: true, // Безкінечна прокрутка
-  slidesPerView: 1,
-  spaceBetween: 10,
-  centeredSlides: true,
-  grabCursor: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  speed: 600,
+document.querySelectorAll(".swiper-container").forEach((container) => {
+  const pagination = container.querySelector(".swiper-pagination");
+  const nextButton = container.querySelector(".swiper-button-next");
+  const prevButton = container.querySelector(".swiper-button-prev");
+
+  new Swiper(container, {
+    modules: [Navigation, Pagination],
+    direction: "horizontal",
+    slidesPerView: 1,
+    spaceBetween: 10,
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 10,
+      },
+      1200: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+    },
+    centeredSlides: true,
+    grabCursor: true,
+    pagination: {
+      el: pagination,
+      clickable: true,
+    },
+    navigation: {
+      nextEl: nextButton,
+      prevEl: prevButton,
+    },
+    speed: 800,
+  });
 });
